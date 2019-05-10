9a6aef05ec23167a38102b304f86ea27ffda5267
Nate Magera <nmagera07@gmail.com>
2019-05-10 12:31:38 -0400

    finished the prototype exercise

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,15 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function sumNums(x, y, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
+}
+
+sumNums(2, 4, function (sum) {
+  console.log(sum)
+})
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +23,33 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(x, y, cb) {
+  
+  return cb(x + y)
+}
+
+consume(2, 4, function (add) {
+  console.log(add)
+})
+
+// Multiply callback function
+function consume(x, y, cb) {
+  
+  return cb(x * y)
+}
+
+consume(2, 4, function (multiply) {
+  console.log(multiply)
+})
+
+//Greeting function
+function consume(firstName, lastName, cb) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
+consume('Mary', 'Poppins', function(greeting) {
+  console.log(greeting)
+})
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
@@ -26,7 +62,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// nestedFunction() can access the variable 'internal' because it can access parent variables. It's a child element, therefore can access the parent's attributes. 
 
 const external = "I'm outside the function";
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,10 +72,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+c
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach( grads => {
+  universities.push(grads.university)
+})
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -84,8 +88,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
-
+graduates.forEach( grads => {
+  contactInfo.push(grads.first_name + " " + grads.email)
+})
+console.log(contactInfo)
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
@@ -114,23 +120,30 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach( animal => {
+  animalNames.push(animal.animal_name + " " + animal.scientific_name)
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the result.
 
 */
 
 const lowerCase = [];
+let allAnimals = zooAnimals.map(animal => animal.animal_name.toLowerCase())
+lowerCase.push(allAnimals)
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
 const largerPopulation = [];
+let lPopulation = zooAnimals.filter(animal => animal.population < 5)
+largerPopulation.push(lPopulation)
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -138,7 +151,12 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = [];
+const calc = (acc, total) => acc + total;
+zooAnimals.forEach(function (animal) {
+    populationTotal.push(animal.population)
+})
+populationTotal = populationTotal.reduce(calc)
 console.log(populationTotal);
 
 
